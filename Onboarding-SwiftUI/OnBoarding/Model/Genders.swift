e9a36822f6b21b561942c8ed39acb5a61a85c5d1
varunbhalla19 <varunbhalla19@gmail.com>
2022-08-15 14:02:20 +0530

    added Genders and ScreenState models

---
+++ Onboarding-SwiftUI/OnBoarding/Model/Genders.swift
@@ -0,0 +1,14 @@
+//
+//  Genders.swift
+//  Onboarding-SwiftUI
+//
+//  Created by varunbhalla19 on 15/08/22.
+//
+
+import Foundation
+
+enum Genders: String {
+    case male = "Male"
+    case female = "Female"
+    case nb = "Non-Binary"
+}
---
+++ Onboarding-SwiftUI/OnBoarding/Model/ScreenStates.swift
@@ -0,0 +1,12 @@
+//
+//  ScreenStates.swift
+//  Onboarding-SwiftUI
+//
+//  Created by varunbhalla19 on 15/08/22.
+//
+
+import Foundation
+
+enum ScreenStates {
+    case welcome, name, age, gender, final, done
+}
