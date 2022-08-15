704f16019ea454add64447d4262f303123616ff8
varunbhalla19 <varunbhalla19@gmail.com>
2022-08-15 12:58:29 +0530

    initial commit

---
+++ Onboarding-SwiftUI/Onboarding_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_SwiftUIApp.swift
+//  Onboarding-SwiftUI
+//
+//  Created by varunb-kc on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
